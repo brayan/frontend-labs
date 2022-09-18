8394bf73b93db10ebcfecfd26ab8d0b61f541919
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-09-18 14:09:15 -0300

    Add initial structure of new js game

---
+++ javascript/free-code-camp/js-game-development/app.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas1')
+const context = canvas.getContext('2d')
+
+const CANVAS_WIDTH = 600
+const CANVAS_HEIGHT = 600
+
+canvas.width = CANVAS_WIDTH
+canvas.height = CANVAS_HEIGHT
+
+
+console.log(context)
